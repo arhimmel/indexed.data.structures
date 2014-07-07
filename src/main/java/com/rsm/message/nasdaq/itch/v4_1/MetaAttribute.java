6970f9d18d14aad26e72f6745ec15b674212a75f
Ariel Himmelstern <arhimmel@gmail.com>
2014-07-07 00:19:16 -0500

    moved itch generated files to xml version only

--- src/main/java/com/rsm/message/nasdaq/itch/v4_1/MetaAttribute.java
+++
@@ -1,9 +0,0 @@
-/* Generated SBE (Simple Binary Encoding) message codec */
-package com.rsm.message.nasdaq.itch.v4_1;
-
-public enum MetaAttribute
-{
-    EPOCH,
-    TIME_UNIT,
-    SEMANTIC_TYPE
-}
